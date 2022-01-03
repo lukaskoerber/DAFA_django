28a9bd6cb03b94db1c13a4cb1fb25ee1a769c40c
lukaskoerber <lukaskoerber11@googlemail.com>
2022-01-03 10:45:13 +0100

    template and views

--- app_1/views.py
+++ app_1/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Message
 
 # Create your views here.
 
 def main_page(request):
-    # Ein request bekommen und ein HTML Dokument zurückgeben
-    # Dabei zurückgreifen auf eine Message die in der Datenbank steht 
-    return render(request, "main_page.html")
+    
+    # get first data-object from sqlite3 database
+    message = Message.objects.first()
+
+    # return the html template and the message instance
+    return render(request, "main_page.html", {'Message': message})
